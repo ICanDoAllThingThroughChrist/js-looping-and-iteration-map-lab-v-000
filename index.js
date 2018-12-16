cf0f62ef88831e0ecd148dc48e31314bc99f96c5
charlie  lee <ctlee9096@gmail.com>
2018-12-16 00:14:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Code your solution in this file.
 function lowerCaseDrivers(drivers) {
   return drivers.map(function(element) { return element.toLowerCase();
-                    )
-                                        }
+  });
 }
